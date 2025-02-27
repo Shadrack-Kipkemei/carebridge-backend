c9fde4c60dc7966644dae60619105862f8ca517e
nikitaamani <nikita.bosire@student.moringaschool.com>
2025-02-27 10:33:03 +0300

    Enhance test suite by adding user fixtures and improving donation tests

--- server/tests/test_donations.py
+++ server/tests/test_donations.py
@@ -1,3 +1,5 @@
+from server.models import Charity, Category, db
+
 def test_create_donation(client):
     """Test creating a donation (Requires Authentication)"""
     # Register and login a donor
@@ -15,9 +17,15 @@ def test_create_donation(client):
     
     access_token = login_res.get_json()["access_token"]
 
+    # Create a charity & category to donate to
+    charity = Charity(name="Help Africa", description="Providing hygiene kits", owner_id=1)
+    category = Category(name="Medical")
+    db.session.add_all([charity, category])
+    db.session.commit()
+
     response = client.post("/donations", json={
-        "charity_id": 1,
-        "category_id": 1,
+        "charity_id": charity.id,
+        "category_id": category.id,
         "amount": 50,
         "donation_type": "one-time"
     }, headers={"Authorization": f"Bearer {access_token}"})
