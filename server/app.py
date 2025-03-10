83dc58d80037619aa28efd0053a9adcea5eb77f3
sktanui <shadrackkemei2019@gmail.com>
2025-03-10 17:16:57 +0300

    Update CORS configuration to include PATCH method

--- server/app.py
+++ server/app.py
@@ -43,7 +43,7 @@ mail = Mail(app)
 s = URLSafeTimedSerializer("your_secret_key")  # Token generator
 CORS(app, resources={r"/*": {
     "origins": ["http://localhost:3000", "https://care-bridge-frontend-200.vercel.app"],
-    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    "methods": ["GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"],
     "allow_headers": ["Content-Type", "Authorization"],
     "supports_credentials": True
 }})
